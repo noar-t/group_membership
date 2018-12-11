ead7ad45aeba878840c046b4a70a8250fe20c5c2
noar-t <noahthornton15@gmail.com>
2018-12-11 17:39:20 -0600

    Added sorted list for message recieving

--- membership/atomic_broadcast/atomic_broadcast.py
+++ membership/atomic_broadcast/atomic_broadcast.py
@@ -1,3 +1,4 @@
+import time
 import multiprocessing as mp
 from membership.atomic_broadcast.channel import Channel, Message
 from membership import LOG
@@ -27,7 +28,7 @@ class AtomicBroadcaster(object):
 
     #Forwards message (T,s,,h+1), on channels c+1,...,f+1-h
     def __forwarder_worker(self):
-        msg = None
+        msg = None 
         recv_time = None
         while True:
             recv_time, msg = self.msg_queue.get()
@@ -48,9 +49,35 @@ class AtomicBroadcaster(object):
         #TODO we can use popen to invoke ping but that may be poor style
         pass
 
-
     # Send message on all channels
     def broadcast(self, message):
         for c in self.channels:
             for host in self.hosts:
                 c.send(host, message)
+
+class MessageList(object):
+    #intermal format should be (time to accept message, message)
+    #output list should just be message objects
+
+    def __init__(self):
+        self.messages = list()
+
+    def get_messages(self):
+        time = time.time()
+        out = list()
+        last_index = 0
+        for i, message in enumerate(self.messages):
+            # if message is ready to be received
+            if message[0] > time:
+                out.append(message[1])
+                last_index = i+1
+            else:
+                break
+        self.messages = self.messages[last_index:]
+        pass
+
+    def add_message(self, new_message):
+        for i, message in enumerate(self.messages):
+            if new_message[0] < message.time[0]:
+                self.messages.insert(i, new_message)
+
