5b79f863b5fea16798492c33501899fb4c359884
William Lin <wlsaidhi@gmail.com>
2018-12-10 21:30:28 -0800

    clusters now support adding and removing servers

--- membership/__main__.py
+++ membership/__main__.py
@@ -4,14 +4,65 @@ from membership import LOG, cli, cluster
 from membership.server import server
 
 
+def add_server(c, *args):
+    """
+    :c: cluster object
+    :id: id of server to add
+    """
+    # FIXME
+    id = int(args[0])
+    c.add(id)
+
+
+def remove_server(c, *args):
+    """
+    :c: cluster object
+    :id: id of server to remove
+    """
+    # FIXME
+    id = int(args[0])
+    LOG.info("removing server %i", id)
+    c.remove(id)
+
+
+def list_servers(c, *args):
+    """
+    lists running servers
+    """
+    print("Servers ids:")
+    print(c)
+
+
+OPTIONS = {
+    'add': add_server,
+    'remove': remove_server,
+    'ls': list_servers,
+}
+
+
 def run_server(args):
     s = server.Server(args)
     s.start()
 
 
 def run_cluster(args):
+    def handle_cmd(c, cmd, *args):
+        try:
+            OPTIONS[cmd](c, *args)
+        except KeyError:
+            print("Command not found.")
+
     c = cluster.Cluster(args)
     c.start()
+    try:
+        while True:
+            cmd = input()
+            cmd = cmd.split()
+            handle_cmd(c, cmd[0], *cmd[1:])
+        # for server in self.servers:
+            # server['process'].wait()
+    except KeyboardInterrupt:
+        c.kill()
 
 
 def main():
--- membership/cluster/__init__.py
+++ membership/cluster/__init__.py
@@ -7,7 +7,7 @@ class Cluster(object):
     """main cluster object"""
 
     def __init__(self, args):
-        self.servers = []
+        self.servers = {}
         self.args = args
 
     def start(self):
@@ -33,21 +33,56 @@ class Cluster(object):
             # spawn child process
             p = Popen(args + id_arg, stdout=log, stderr=log)
 
-            self.servers.append({'process': p, 'log': log})
+            self.servers[id] = {'process': p, 'log': log}
 
-        # TODO add cli support
-        try:
-            # wait on servers
-            for server in self.servers:
-                server['process'].wait()
-        except KeyboardInterrupt:
-            self.kill()
+    def add(self, id):
+        """
+        adds server id to the cluster
+        :id: new server's id
+        """
+        if id in self.servers:
+            LOG.error("server %i already exists", id)
+            return False
+        elif id < 0 or id > 100:
+            LOG.error("id %i is out of range [0, 100]", id)
+            return False
+
+        # TODO clean up
+        args = ['python3', '-m', 'membership', 'server', '-c',
+                str(self.args.count)]
+
+        if self.args.verbose:
+            args.append('-v')
+
+        id_arg = ['-i', str(id)]
+        # open log file
+        log = open(os.path.join('logs', str(id) + '.log'), 'w')
+        LOG.info('starting server: %i', id)
+        # spawn child process
+        p = Popen(args + id_arg, stdout=log, stderr=log)
+        self.servers[id] = {'process': p, 'log': log}
+        return True
+
+    def remove(self, id):
+        """
+        removes server id from the cluster
+        :id: server id to remove
+        """
+        if id not in self.servers:
+            LOG.error("server %i does not exists", id)
+            return False
+        LOG.info("forcibly termination server %i", id)
+        self.servers[id]['process'].kill()
+        del self.servers[id]
 
     def kill(self):
         """terminate cluster"""
 
-        for server in self.servers:
+        for _, server in self.servers.items():
             server['process'].kill()
             server['log'].close()
 
         LOG.info('cluster killed')
+
+    def __str__(self):
+        return '\n'.join([str(id) for id in self.servers])
