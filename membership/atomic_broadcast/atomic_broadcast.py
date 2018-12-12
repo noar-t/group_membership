33025491af7c0649723b643fff79533b1ae10633
William Lin <wlsaidhi@gmail.com>
2018-12-11 23:54:08 -0800

    wip: broadcast figure out channels to forward on how to test/benchmark how to simulate failures unittests?

--- membership/atomic_broadcast/atomic_broadcast.py
+++ membership/atomic_broadcast/atomic_broadcast.py
@@ -1,3 +1,4 @@
+import functools
 import time
 import threading as th
 import multiprocessing as mp
@@ -16,7 +17,10 @@ class AtomicBroadcaster(object):
         """
         self.msg_queue = mp.Queue()
         self.hosts = hosts
+        self.channel_count = channel_count
         self.server_port = server_port
+        self.c_hist_lock = th.Lock()
+        self.c_history = {}
         self.channels = [Channel(server_port, n + 1, self.msg_queue)
                          for n in range(channel_count)]
         LOG.info("atomicbroadcaster created")
@@ -29,23 +33,43 @@ class AtomicBroadcaster(object):
         self.__forwarder.start()
 
         # XXX
-        # m = Message(b'123451234512345', b'HI', 0)
+        m = Message(b'123451234512345', b'HI', 0)
 
-        # time.sleep(2)
-        # self.broadcast(m)
+        time.sleep(2)
+        self.broadcast(m)
+
+    def __add_to_c_history(self, key, msg):
+        self.c_hist_lock.acquire()
+        if key not in self.c_history:
+            self.c_history[key] = {'msg': msg, 'c': self.channel_id}
+        elif self.c_history[key]['c'] < msg.chan:
+            self.c_history[key]['c'] = msg.chan
+        self.c_hist_lock.release()
 
     # Forwards message (T,s,,h+1), on channels c+1,...,f+1-h
     def __forwarder_worker(self):
         msg = None
         recv_time = None
         while True:
-            recv_time, binary_msg = self.msg_queue.get()
-            msg = Message(None, binary_msg, None, True)
-            if msg.is_timely(recv_time, self.sigma):
-                self.message_list.add_message(accept_time, msg)
-                self.__forward(msg)
+            msg = self.msg_queue.get()
+            # msg = Message(None, binary_msg, None, True)
+            if not msg.is_late(recv_time, self.channel_count // 2, self.sigma):
+                if msg.is_timely(recv_time, self.sigma):
+                    # self.message_list.add_message(accept_time, msg)
+                    key = (msg.time, addr)
+                    self.__add_to_c_history(key, msg)
 
-    def __forward(self, msg):
+                    # schedule forwarding task
+                    self.__schedule_forward_task(msg)
+
+
+
+    def __schedule_forward_task(self, msg):
+        forward_task = functools.partial(self.__forward_task, msg=msg)
+        task_thread = th.Thread(target=forward_task)
+        task_thread.daemon = True
+        task_thread.start()
+
         LOG.info("forwarding msg from %i", self.server_port)
         c = msg.chan
         h = msg.hops
@@ -54,6 +78,17 @@ class AtomicBroadcaster(object):
             chan = self.channels[channel_id]
             for _, host in self.hosts.items():
                 chan.send(host.ip, host.port, msg)
+
+    def __forward_task(self, msg):
+        # TODO: make this atomic?
+        duration = msg.get_timely_deadline(self.sigma) - time.time()
+        time.sleep(duration)
+
+        # determine the channels to forward msg on
+        self.c_hist_lock.acquire()
+
+        self.c_hist_lock.release()
+
 
     def calc_sigma(self):
         """ Find average ping to all hosts """
--- membership/atomic_broadcast/channel.py
+++ membership/atomic_broadcast/channel.py
@@ -45,26 +45,38 @@ class Channel(object):
     def __recv_worker(self):
         """ Recieves messages and places them in the output queue """
         # messages are sized due to struct layout
-        data, addr = self.socket.recvfrom(1080)
-        LOG.debug("received message from: %s on c%i", addr, self.channel_id)
-        self.msg_queue.put((time.time(), data))
-        while data:
-            data, addr = self.socket.recvfrom(1080)
-            LOG.debug("received message from: %s on c%i", addr, self.channel_id)
-            self.msg_queue.put((time.time(), data))
+
+        while True:
+            binary_msg, addr = self.socket.recvfrom(1080)
+            msg = Message.from_binary_msg(binary_msg, addr)
+            LOG.debug("received message from: %s on c%i", addr,
+                      msg.chan)
+            self.msg_queue.put(msg)
 
 
 class Message(object):
-    def __init__(self, host, data, chan, copy=None):
-        if copy is None:
+    def __init__(self, host, data, chan, copy=False):
+        if not copy:
             self.time = None  # float
             self.hops = 1  # int
             self.host = host  # should be parsed to 15 char array
             self.chan = chan
             self.data = data  # 1024 byte buffer
+
+            self.recv_time = None
+            self.addr = None
         else:
             # copy constructor/unmarshal
             self.unmarshal(data)
+
+    @classmethod
+    def from_binary_msg(cls, binary_msg, addr):
+        """ Initialize Message from a binary blob """
+        msg = cls(None, binary_msg, None, copy=True)
+        msg.recv_time = time.time()
+        msg.addr = addr
+
+        return msg
 
     def marshal(self):
         """ Turns a message object into an transmittable format """
@@ -85,8 +97,14 @@ class Message(object):
     def add_hop(self):
         self.hops += 1
 
-    def is_timely(self, time, sigma):
+    def is_late(self, time, k, sigma):
+        return time >= self.time + (k + 1) * sigma
+
+    def is_timely(self, sigma):
         """ Determines if a message is Timely U < T +h(δ+ε) """
-        LOG.debug("is_timely: %f < (%f + %i + %f)", time, self.time, self.hops,
-                sigma)
-        return time < (self.time + self.hops * sigma)
+        # LOG.debug("is_timely: %f < (%f + %i + %f)",
+        # time, self.time, self.hops, sigma)
+        return self.recv_time < (self.time + self.hops * sigma)
+
+    def get_timely_deadline(self, sigma):
+        return self.time + self.hops * sigma
